4297f5417a6d20a7b99803570ae7394c366e05d8
deniaux <deniaux.emmanuel@gmail.com>
2022-05-13 14:30:23 +0200

    final

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:8-jdk-alpine
 
-COPY target/gs-rest-service-0.2.0.jar  app.jar
+COPY target/gs-rest-service-0.3.0.jar  app.jar
 
 EXPOSE 8080
 ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,5 +1,6 @@
 package hello;
-
+import java.util.*;
+import java.sql.*;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,25 @@ public class GreetingController {
 
     @RequestMapping("/movies")
     public String movies(){
-        return "movies";
+    	List<String> listMovies = new ArrayList<String>();
+	String sqlSelectAllMovies = "SELECT * FROM movies";
+	String connectionUrl="jdbc:mysql://127.0.0.1:3306/movies?serverTimezone=UTC";
+
+	try(Connection conn = DriverManager.getConnection(connectionUrl, "root","admin");
+		PreparedStatement ps = conn.prepareStatement(sqlSelectAllMovies);
+		ResultSet rs = ps.executeQuery()){
+
+		while(rs.next()) {
+		System.out.println("ICI");
+			String title = rs.getString("title");
+		listMovies.add(title);
+		}
+
+	 }catch(SQLException throwables){
+	 	throwables.printStackTrace();
+	 }
+
+	
+	    return listMovies.toString();
     }
 }
